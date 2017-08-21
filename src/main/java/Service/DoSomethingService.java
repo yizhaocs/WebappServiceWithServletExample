c1c7aac77dc5b02839079528f72d2bc8f9ea6ae4
yzhao <yi.zhao@adara.com>
2017-08-21 10:43:34 -0700

    1

---
+++ src/main/java/Service/DoSomethingService.java
@@ -0,0 +1,9 @@
+package Service;
+
+/**
+ * Created by yzhao on 8/21/17.
+ */
+public interface DoSomethingService {
+    public void find() throws Exception;
+    public void create() throws Exception;
+}
---
+++ src/main/java/Service/DoSomethingServiceImpl.java
@@ -0,0 +1,35 @@
+package Service;
+
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by yzhao on 8/21/17.
+ */
+public class DoSomethingServiceImpl implements DoSomethingService{
+    private List<String> somethingList;
+
+    public void init(){
+        System.out.println("init something");
+    }
+
+    public void destroy(){
+        System.out.println("destory something");
+    }
+
+    public void find() throws Exception{
+        System.out.println("find something");
+    }
+    public void create() throws Exception{
+        System.out.println("create something");
+    }
+
+    public List<String> getSomethingList() {
+        return somethingList;
+    }
+
+    public void setSomethingList(List<String> somethingList) {
+        this.somethingList = somethingList;
+    }
+}
