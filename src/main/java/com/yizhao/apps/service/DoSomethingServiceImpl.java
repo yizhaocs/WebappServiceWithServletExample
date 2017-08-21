30a0bdd73332fb1d2f284a879d141f81a6077af0
yzhao <yi.zhao@adara.com>
2017-08-21 11:37:28 -0700

    1

--- src/main/java/com/yizhao/apps/service/DoSomethingServiceImpl.java
+++ src/main/java/com/yizhao/apps/service/DoSomethingServiceImpl.java
@@ -4,6 +4,7 @@ package com.yizhao.apps.service;
 import org.apache.log4j.Logger;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yzhao on 8/21/17.
@@ -11,10 +12,15 @@ import java.util.List;
 public class DoSomethingServiceImpl implements DoSomethingService{
     private static final Logger log = Logger.getLogger(DoSomethingServiceImpl.class);
 
+    private String somethingValue;
     private List<String> somethingList;
+    private Map<String, String> somethingMap;
 
     public void init(){
-        System.out.println("init something");
+        System.out.println("[DoSomethingServiceImpl.init] ");
+        System.out.println("[DoSomethingServiceImpl.init] somethingValue:" + somethingValue);
+        System.out.println("[DoSomethingServiceImpl.init] somethingList:" + somethingList);
+        System.out.println("[DoSomethingServiceImpl.init] somethingMap:" + somethingMap);
     }
 
     public void destroy(){
@@ -28,11 +34,27 @@ public class DoSomethingServiceImpl implements DoSomethingService{
         System.out.println("create something");
     }
 
+    public String getSomethingValue() {
+        return somethingValue;
+    }
+
+    public void setSomethingValue(String somethingValue) {
+        this.somethingValue = somethingValue;
+    }
+
     public List<String> getSomethingList() {
         return somethingList;
     }
 
     public void setSomethingList(List<String> somethingList) {
         this.somethingList = somethingList;
+    }
+
+    public Map<String, String> getSomethingMap() {
+        return somethingMap;
+    }
+
+    public void setSomethingMap(Map<String, String> somethingMap) {
+        this.somethingMap = somethingMap;
     }
 }
